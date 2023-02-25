9e331e02f7b45149069d08505ee28868463a01f5
Daniel Puckowski <puckowski.d@gmail.com>
2023-02-25 11:45:52 -0500

    Update cront tasks for Cleanbase 1.1
    * Update cron tasks for Cleanbase version 1.1. Change how endpoints are
      rebuilt on system boot.

--- rebuildall.sh
+++ rebuildall.sh
@@ -1,4 +1,13 @@
 echo "Rebuild..."
+
+while ! mysqladmin ping -h"localhost" --silent; do
+    sleep 1
+done
+
+while [[ $(systemctl show --property ActiveState docker) != "ActiveState=active" ]]; do
+    sleep 1
+done
+
 MYPATH=`dirname "$0"`
 cd "$MYPATH"
 sudo node "./rebuildservices.js"
